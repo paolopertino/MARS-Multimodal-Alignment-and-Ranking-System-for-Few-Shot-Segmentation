d65b8842505f6182617d8b90482c6d8b6fe20d08
Paolo Pertino <pertino.paolo@gmail.com>
2025-04-14 00:03:36 +0200

    [Pages] Forced light theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
     shikiConfig: {
       themes: {
         light: 'github-light',
-        dark: 'github-dark',
+        // dark: 'github-dark',
       },
     }
   }
