5a46ed8cc03f3216e225ec51ac7adec83f95ea1e
Paolo Pertino <pertino.paolo@gmail.com>
2025-04-16 09:14:12 +0200

    [MARS] added test scripts

---
+++ scripts/coco_5shot.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+for fold in 0 1 2 3;
+do
+  python main_MARS.py  \
+    --dataset_path /home/pertino/exp/private_datasets \
+    --annotations_datapath /home/pertino/exp/private_datasets/COCO2014/annotations \
+    --nltk_path /home/pertino/exp/NLTK_data \
+    --mask_proposals_path /home/pertino/exp/MARS-Multimodal-Alignment-and-Ranking-System-for-Few-Shot-Segmentation/vis/coco_test/5shot/coco_test_pred_masks_unmerged_unfiltered/ \
+    --benchmark coco \
+    --nworker 0 \
+    --nshot 5 \
+    --fold ${fold} \
+    --input_size 518 \
+    --models_path /home/pertino/exp/MARS-Multimodal-Alignment-and-Ranking-System-for-Few-Shot-Segmentation/models \
+    --prompt_type contour \
+    --zoom_percentage 50 \
+    --color red \
+    --alpha_blending 0.5 \
+    --thickness 2 \
+    --vlm4bit \
+    --vta_backbone 'ViT-B/16' \
+    --vta_refinement_box_threshold 0.4 \
+    --last_n_attn_for_vta_refinement 8 \
+    --vva_backbone dino \
+    --dino_backbone vit_large \
+    --num_regs 4 \
+    --vva_refinement_box_threshold 0.8 \
+    --last_n_attn_for_vva_refinement 24 \
+    --use_vva_mix \
+    --use_negative_prior \
+    --static_threshold 0.55 \
+    --dynamic_threshold 0.95 \
+    --alpha_coverage 0.85 \
+    --log_root_path "/home/pertino/exp/MARS-Multimodal-Alignment-and-Ranking-System-for-Few-Shot-Segmentation/output/mars/coco/fold${fold}" \
+    --exp_name 5shot
+done
+wait
